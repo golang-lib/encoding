e56ad1b8f824637299c371613b2085b051468d77
chai2010 <chaishushan@gmail.com>
2014-11-21 10:12:55 +0800

    add protobuf

--- gen.go
+++ gen.go
@@ -2,5 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package encoding provides encoding extension drivers.
+//go:generate go run gen_helper.go
+
 package encoding
